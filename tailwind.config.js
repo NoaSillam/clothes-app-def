08a7c3a9f726232942afbab0dd82ef0cd25d508a
Noa Sillam <noasillam@gmail.com>
2024-10-31 16:25:52 +0100

    commit-init

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx, res, resi}", // Assure-toi d'inclure les fichiers ReScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
